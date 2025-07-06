443714320574196e04ff449485edc3de56c2750c
swastik2207 <chatterjee.swastik022@gmail.com>
2025-07-06 21:58:31 +0530

    few details changed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'wasteDetection',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Swastik Chatterjee',
+    author_email= 'chatterjee.swastik022@gmail.com',
     packages= find_packages(),
     install_requires = []
 
